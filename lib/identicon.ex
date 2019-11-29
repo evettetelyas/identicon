ed7ed0c980c77ead31ae1d3f71440754daa59804
Julien Bisconti <julienbi@unity3d.com>
2019-11-29 14:53:59 -0700

    refactor filter_odd_squares

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -34,9 +34,10 @@ defmodule Identicon do
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
-    grid = Enum.filter grid, fn({code, _index}) -> 
-      rem(code, 2) == 0
-    end
+    grid = 
+      Enum.filter grid, fn({code, _index}) -> 
+        rem(code, 2) == 0
+      end
 
     %Identicon.Image{image | grid: grid}
   end
